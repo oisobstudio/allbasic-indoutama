111753bee6eddcf87cb5fd0d943a7b19845a083c
oisobstudio <oisob.studio@zoho.com>
2017-03-10 01:27:22 +0700

    remove tornado in URL

--- dms/urls.py
+++ dms/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-import tornado.web
 
 
 urlpatterns = [
