49770ddcc2f6c7063bb1ce638ecae9b4c5e8d137
oisobstudio <oisob.studio@zoho.com>
2017-03-14 02:15:36 +0700

    Mendambahkan admin untuk article

--- aplinventory/admin.py
+++ aplinventory/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import SizeCategory
+from .models import SizeCategory, Article, ArticleDetail
+
+class AdminArticleDetailInline(admin.TabularInline):
+	model = ArticleDetail
+
+class AdminArticle(admin.ModelAdmin):
+	list_display = ('user', 'product', 'code', 'name', 'capital_price', 'price',)
+	inlines = [
+		AdminArticleDetailInline,
+	]
 
 admin.site.register(SizeCategory)
-
+admin.site.register(Article, AdminArticle)
 # Register your models here.
